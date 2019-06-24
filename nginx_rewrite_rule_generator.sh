351947109d38aa34be17f98b6fd6f284a4f9aea9
Valentin Dzhorov <dzhorov@delta.bg>
2019-06-24 12:23:45 +0300

    Improved the script nginx_rewrite_rule_generator.sh:
    Added a function that automatically replaced special characters such as
    [, ], {, } with the same char, but with preceeding \. Example:
    
    \[.

--- nginx_rewrite_rule_generator.sh
+++ nginx_rewrite_rule_generator.sh
@@ -8,11 +8,14 @@
 # rewrite ^/example.com/old/location$ https://example.com/new/location permanent;
 # Everything will be outputed to a file which you can configure via $OUTPUT_FILE.
 # The script takes one arguement - a file. Usage: script.sh /path/to/file.txt.
+# The array ESCAPE_CHARS hold characters which are replaced by the same character,
+# but preceeded with /. This is needed as Nginx otherwise interprets them as environment variables.
 # 
 # Author: Valentin Dzhorov
 
 FILE="$1"
 OUTPUT_FILE='rewrite_rules.txt'
+ESCAPE_CHARS=(']' '[' '}' '{')
 
 if [ ! -f "$FILE" ]; then
   echo "File $FILE not found. Exiting."
@@ -24,15 +27,23 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
+function string_replace () {
+  for i in "${ESCAPE_CHARS[@]}"; do
+  sed -r -i "s/\\${i}/\/\\${i}/g" $1
+  done
+}
+
 function convert () {
   local file_input=$1;
   while IFS= read -r line
   do
   local domain=$(echo $line | awk '{print $1}' | cut -d '/' -f1-3)
-  echo -n 'rewrite ^' 
-  echo -n "$line" | awk '{print $1}' | awk -F "$domain" 'BEGIN {ORS=" "} BEGIN {OFS=""} {print $2, "$"}'
+  echo -n 'rewrite ^'
+  local line1=$(echo "$line" | awk '{print $1}' | awk -F "$domain" 'BEGIN {ORS=" "} BEGIN {OFS=""} {print $2, "$"}')
+  echo -n "$line1 "
   echo -n "$line" | awk 'BEGIN {OFS=""} {print $2, " permanent" , ";"}'
   done < "$file_input"
 }
 
 convert $FILE > $OUTPUT_FILE
+string_replace $OUTPUT_FILE
