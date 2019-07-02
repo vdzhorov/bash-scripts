0c884fca2a895ed34bd596411a187bc194d5f14a
Valentin Dzhorov <dzhorov@delta.bg>
2019-07-02 12:14:09 +0300

    Updated the script and added ability to generate maps:
    * Updated the readme at the beginning
    * Added new function which generates nginx maps
    * The old function which is used for generating rewrites is still present.
    Whether you want maps or rewrites is controlled via $1.

--- nginx_rewrite_rule_generator.sh
+++ nginx_rewrite_rule_generator.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 #
-# This script was used for bulk-generating Nginx 301 redirects from a txt file.
-# The format of the txt file where the domains are places are as follows:
+# Usage of the script: ./nginx_rewrite_rule_generator.sh {map|rewrite} urls.txt, where:
+# the first parameter (map|rewrite) indicates what you want to generate - nginx rewrites or maps.
+# Warning: Rewrites do not work for URIs that contain query string. If you have such URIs then maybe
+# you should look into generating nginx maps using this script.
+#
+# This script will bulk-generate Nginx 301 redirects from a txt input file.
+# The format of the txt file where the domains are places is as follows:
 # <Old URL>                        <New URL>
 # https://example.com/old/location https://example.com/new/location
-# This will result in the following:
+# 
+# This will result in the following in case you want rewrites:
 # rewrite ^/example.com/old/location$ https://example.com/new/location permanent;
+# or in case you want maps:
+# map $request_uri $new_uri {
+#     default "";
+#     /old/locationl     /new/location;
+# }
+#
 # Everything will be outputed to a file which you can configure via $OUTPUT_FILE.
-# The script takes one arguement - a file. Usage: script.sh /path/to/file.txt.
 # The array ESCAPE_CHARS hold characters which are replaced by the same character,
-# but preceeded with /. This is needed as Nginx otherwise interprets them as environment variables.
+# but preceeded with / (used for rewrites, not maps). This is needed as Nginx otherwise interprets them as environment variables.
 # 
 # Author: Valentin Dzhorov
 
-FILE="$1"
+FILE="$2"
 OUTPUT_FILE='rewrite_rules.txt'
 ESCAPE_CHARS=(']' '[' '}' '{')
 
@@ -22,8 +33,8 @@ if [ ! -f "$FILE" ]; then
   exit 1
 fi
 
-if [ "$#" -ne 1 ]; then
-  echo "Illegal number of parameters. Usage: $0 $FILE"
+if [ "$#" -ne 2 ]; then
+  echo "Illegal number of parameters. Usage: $0 {map|rewrite} $FILE"
   exit 1
 fi
 
@@ -33,7 +44,7 @@ function string_replace () {
   done
 }
 
-function convert () {
+function convert_to_rewrites () {
   local file_input=$1;
   while IFS= read -r line
   do
@@ -45,5 +56,28 @@ function convert () {
   done < "$file_input"
 }
 
-convert $FILE > $OUTPUT_FILE
-string_replace $OUTPUT_FILE
+function convert_to_map () {
+  local file_input=$1;
+  echo 'map $request_uri $new_uri {'
+  echo '    default "";'
+  while IFS= read -r line
+  do
+  echo "    $line;"
+  done < "$file_input"
+  echo '}'
+}
+
+case "$1" in
+  rewrite)
+    convert_to_rewrites $FILE > $OUTPUT_FILE
+    string_replace $OUTPUT_FILE
+    ;;
+
+  map)
+    convert_to_map $FILE > $OUTPUT_FILE 
+    ;;
+
+  *)
+    echo "Usage: $0 {map|rewrite} $FILE"
+    exit 1
+esac
